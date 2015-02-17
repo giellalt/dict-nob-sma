47645e3b8a25ef5066f24014f0e51d5ed1582a10
Trond Trosterud <trond.trosterud@uit.no>
2015-02-17 06:30:36 +0000

    use tmpfile and get a no-hands compilation.

--- make-nobsma.sh
+++ make-nobsma.sh
@@ -1,14 +1,42 @@
 
-# Dette skal bli ei makefil for å lage nobsma.fst, 
-# ei fst-fil som tar nob og gjev ei sma-omsetjing.
+echo ""
+echo ""
+echo "---------------------------------------------------"
+echo "Shellscript to make a transducer of the dictionary."
+echo ""
+echo "It writes a lexc file to bin, containing the line	 "
+echo "LEXICON Root										 "
+echo "Thereafter, it picks lemma and first translation	 "
+echo "of the dictionary, adds them to this lexc file,	 "
+echo "and compiles a transducer bin/nobsma.fst		 "
+echo ""
+echo "Usage:"
+echo "lookup bin/nobsma.fst"
+echo "(or if your alias is set up: just write nobsma)"
+echo "---------------------------------------------------"
+echo ""
+echo ""
 
-# Førebels er det berre eit shellscript.
-
-# Kommando for å lage nobsma.fst
 echo "LEXICON Root" > bin/nobsma.lexc
-cat src/*_nobsma.xml | tr '\n' '™' | sed 's/<e>/£/g;'| tr '£' '\n'| sed 's/<re>[^>]*>//g;'|tr '<' '>'| cut -d">" -f5,15| tr ' ' '_'| tr '>' ':'| grep -v '__'|sed 's/$/ # ;/g' >> bin/nobsma.lexc
-xfst -e "read lexc < bin/nobsma.lexc"
 
-# deretter i xfst:
-# invert
-# save bin/nobsma.fst
+cat src/*_nobsma.xml   | \
+tr '\n' '™'            | \
+sed 's/<e>/£/g;'       | \
+tr '£' '\n'            | \
+sed 's/<re>[^>]*>//g;' | \
+tr '<' '>'             | \
+cut -d">" -f5,15       | \
+tr ' ' '_'             | \
+tr '>' ':'             | \
+grep -v '__'           | \
+sed 's/$/ # ;/g'       >> bin/nobsma.lexc
+
+
+
+
+printf "read lexc < bin/nobsma.lexc \n\
+invert net \n\
+save stack bin/nobsma.fst \n\
+quit \n" > tmpfile
+xfst -utf8 < tmpfile
+rm -f tmpfile
